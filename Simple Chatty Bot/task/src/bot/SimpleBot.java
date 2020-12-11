a91f87acd99d0039c3090e9925f9757b47f21a2c
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-11 14:57:28 +0530

    Stage 5/5: The student and the teacher - Implementation Completed

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -3,35 +3,62 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Delta", "2019"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
 
+    static void remindName() {
         String name = scanner.nextLine();
-
         System.out.println("What a great name you have, " + name + "!");
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+    }
 
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
-
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-
-        // read a number and count to it here
         int num = scanner.nextInt();
-
-        for (int i = 0; i < num+1; i++){
-            System.out.println(i + "!");
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
         }
+    }
 
-        System.out.println("Completed, have a nice day!");
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Which statement is the most complete about break and continue?");
+        System.out.println("1. The keyword break can be written in a body of while and do-while loops.");
+        System.out.println("2. The keyword continue cannot be written in a body of while and do-while loops.");
+        System.out.println("3. Both keywords can be written in a body of for, while and do-while loops.");
+        System.out.println("4. The keywords break and continue can be written in the conditional statement if-else.");
+        int answer = 3;
+        int guess = scanner.nextInt();
+        while (guess != answer){
+            System.out.println("Please, try again.");
+            guess = scanner.nextInt();
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
 }
