18be925772dc943013cda5b3f3b5062de54fd42c
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-11 14:44:28 +0530

    Stage 4/5: Let’s count - Implementation Completed

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -16,12 +16,22 @@ public class SimpleBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
-        int divAgeBy3 = scanner.nextInt();
-        int divAgeBy5 = scanner.nextInt();
-        int divAgeBy7 = scanner.nextInt();
-        int yourAge = ((divAgeBy3 % 3) * 70 + (divAgeBy5 % 5) * 21 + (divAgeBy7 % 7) * 15) % 105;
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        System.out.println("Your age is "+ yourAge + "; that's a good time to start programming!");
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+        int num = scanner.nextInt();
+
+        for (int i = 0; i < num+1; i++){
+            System.out.println(i + "!");
+        }
+
+        System.out.println("Completed, have a nice day!");
     }
 }
