353c0635e509d14fad51c5849a20c3aa028df80f
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-11 12:44:54 +0530

    Stage 1/5: Hello! What’s your name - Implementation Completed

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -4,17 +4,10 @@ import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
-        //Scanner scanner = new Scanner(System.in);
-
-        //String botName = scanner.nextLine();
-        //int birthYear = scanner.nextInt();
-
         String botName = "Aid";
         int birthYear = 2020;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
-
     }
 }
